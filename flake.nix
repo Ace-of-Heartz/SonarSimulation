20cfd93679b9384a789995ec8d21a1f791c9e923
Ace-of-Heartz <synthetace@gmail.com>
2025-02-12 19:45:56 +0100

    Added flakes and direnv files

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{ 
+  description = "Julia project flake";
+
+  inputs = {
+    nixgl.url = "github:nix-community/nixGL";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    scientific-fhs.url =  "github:olynch/scientific-fhs";
+  };
+  outputs = { self, nixgl, nixpkgs, scientific-fhs }:
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system; 
+        overlays = [nixgl.overlay];
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nixgl.nixGLIntel
+        ];
+
+        packages = with pkgs; [
+          julia-bin
+          gr-framework
+          stdenv.cc.cc.lib qt5.qtbase qt5Full libGL
+          glxinfo
+          glfw
+        ];
+
+        shellHook = ''
+          nixGLIntel julia
+        '';
+      };
+      
+    };
+}
